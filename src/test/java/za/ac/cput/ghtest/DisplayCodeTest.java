5c12f19673c882b8daab251d65f20ad12c3367e5
218250142 <81311636+218250142@users.noreply.github.com>
2021-03-31 16:01:35 +0200

    Update DisplayCodeTest.java

--- src/test/java/za/ac/cput/ghtest/DisplayCodeTest.java
+++ src/test/java/za/ac/cput/ghtest/DisplayCodeTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -37,11 +38,86 @@ public class DisplayCodeTest {
     @AfterEach
     public void tearDown() {
     }
+    @Test // Kevin Mombo
+    public void Failingtest() {
+        fail("Hello Everyone ");
+    }
+    @Test //kevin Mombo
+    public void testEquality(){
+        String obj1 = "Junit";
+        String obj2 = "Junit";
+        assertEquals(obj1, obj2);
+    }
+    @Test //Kevin Mombo
+    public void testIdentity(){
+        String obj3 = "Kevin";
+        String obj4 = "Kevin";
+        assertSame(obj3, obj4);
+    }
+    @Test //Kevin Mombo
+    @Disabled
+    public void disabledTest(){
+     int a = 12;
+     int b = 10;
+     int sum = a + b;
+        System.out.println("The Sum of a + b = " +sum);
+    }
+    /**
+ *
+ * @author Levi Williams Student Number 218250142
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+ */
+public class AnimalTest {
+    
+    private Animal animal1;
+    private Animal animal2;
+    private Animal animal3;
+    
+    public AnimalTest() {
     }
     
+    
+    
+    @BeforeEach
+    public void setUp() {
+        animal1 = new Animal();
+        animal2 = new Animal();
+        animal3 = animal1;
+    }
+    
+    @Test
+    public void testEquality(){
+    assertEquals(animal1,animal3);
+    }
+    
+    @Test
+    public void testIdentity(){
+    assertSame(animal1,animal3);
+    }
+    
+    @Test
+    @Disabled
+    public void testSetAnimal(){
+    System.out.println("Ignore Test");
+    }
+    
+    @Test
+    public void testGetAnimal(){
+        fail("This will fail");
+    assertEquals(animal1,animal3);
+    }
+    
+   
+    
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
 }
