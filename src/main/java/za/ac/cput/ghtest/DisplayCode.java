eb52f417e87ea09b70142a4af9cec1a637d1d1ec
218250142 <218250142@mycput.ac.za>
2021-04-01 11:55:20 +0200

    commit

--- src/main/java/za/ac/cput/ghtest/DisplayCode.java
+++ src/main/java/za/ac/cput/ghtest/DisplayCode.java
@@ -11,6 +11,33 @@ package za.ac.cput.ghtest;
  * 
  * PLEASE ENTER YOUR CODE BELOW!
  */
+/**
+ *
+ * @author Levi 
+ * Student Number 218250142
+ */
+public class Animal {
+    private String species;
+
+    /**
+     * @return the species
+     */
+    public String getSpecies() {
+        return species;
+    }
+
+    /**
+     * @param species the species to set
+     */
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "species=" + species + '}';
+    }
+}
 public class DisplayCode {
     
 }
--- src/test/java/za/ac/cput/ghtest/DisplayCodeTest.java
+++ src/test/java/za/ac/cput/ghtest/DisplayCodeTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
+ *@author Levi Williams Student Number 218250142
  * @author Kevin Mombo 
  Student branch
  * ENTER CODE BELOW!
@@ -62,5 +62,44 @@ public class DisplayCodeTest {
      int b = 10;
      int sum = a + b;
         System.out.println("The Sum of a + b = " +sum);
+    }
+public class AnimalTest {
+    
+    private Animal animal1;
+    private Animal animal2; //Levi Williams Student Number 218250142
+    private Animal animal3;
+    
+    public AnimalTest() {
+    }
+    
+    
+    
+    @BeforeEach
+    public void setUp() {
+        animal1 = new Animal();//Levi Williams Student Number 218250142
+        animal2 = new Animal();
+        animal3 = animal1;
+    }
+    
+    @Test
+    public void testEquality(){
+    assertEquals(animal1,animal3);//Levi Williams Student Number 218250142
+    }
+    
+    @Test
+    public void testIdentity(){
+    assertSame(animal1,animal3);//Levi Williams Student Number 218250142
+    }
+    
+    @Test
+    @Disabled
+    public void testSetAnimal(){
+    System.out.println("Ignore Test");//Levi Williams Student Number 218250142
+    }
+    
+    @Test
+    public void testGetAnimal(){
+        fail("This will fail");//Levi Williams Student Number 218250142
+    assertEquals(animal1,animal3);
     }
 }
